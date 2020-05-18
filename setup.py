0e46744ba2148d6a9ab246d80cad85edc593be1e
Daniel Kang <daniel.d.kang@gmail.com>
2020-05-18 01:21:57 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+        name='supg',
+        version='0.0.1',
+        packages=['supg']
+)
---
+++ supg/datasource/__init__.py
@@ -0,0 +1,3 @@
+from .datasource import DataSource, DFDataSource
+from .beta import BetaDataSource
+from .csv_source import *
