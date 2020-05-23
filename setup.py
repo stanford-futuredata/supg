d3dabd93af75b22073ef4b69355565dc23bd8285
Daniel Kang <daniel.d.kang@gmail.com>
2020-05-23 14:34:59 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from distutils.core import setup
 setup(
         name='supg',
         version='0.0.1',
+        author='Daniel Kang, Edward Gan',
+        author_email='ddkang@stanford.edu',
         packages=['supg']
 )
